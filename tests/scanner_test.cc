06623893063c71363a7e64786b9613dca3cb4030
Nimor111 <georgi.bojinov@hotmail.com>
2019-01-28 01:00:07 +0200

    Add test for function decl

--- tests/scanner_test.cc
+++ tests/scanner_test.cc
@@ -30,4 +30,33 @@ TEST_CASE("Scanner", "[scanner]")
         auto tokens = scanner.ScanTokens();
         REQUIRE(tokens == expectedTokens);
     }
+
+    SECTION("Function definition")
+    {
+        std::ifstream file("tests/fixtures/function.lua");
+
+        std::stringstream buffer;
+        buffer << file.rdbuf();
+
+        Scanner scanner(buffer.str());
+
+        std::vector<Token> expectedTokens {
+            Token(FUNCTION, "function", "", 1),
+            Token(IDENT, "sum", "", 1),
+            Token(LEFT_PAREN, "(", "", 1),
+            Token(IDENT, "a", "", 1),
+            Token(COMMA, ",", "", 1),
+            Token(IDENT, "b", "", 1),
+            Token(RIGHT_PAREN, ")", "", 1),
+            Token(RETURN, "return", "", 2),
+            Token(IDENT, "a", "", 2),
+            Token(PLUS, "+", "", 2),
+            Token(IDENT, "b", "", 2),
+            Token(END, "end", "", 3),
+            Token(T_EOF, "", "", 4)
+        };
+
+        auto tokens = scanner.ScanTokens();
+        REQUIRE(tokens == expectedTokens);
+    }
 }
